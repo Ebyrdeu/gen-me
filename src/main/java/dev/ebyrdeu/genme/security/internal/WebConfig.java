c24cab197c3df48f011fa856e7f248fbd8decbe0
Maxim Khnykin <ebyrdeu.khnykin@gmail.com>
2025-05-06 15:40:29 +0000

    updated

--- src/main/java/dev/ebyrdeu/genme/security/internal/WebConfig.java
+++ src/main/java/dev/ebyrdeu/genme/security/internal/WebConfig.java
@@ -1,38 +1,16 @@
 package dev.ebyrdeu.genme.security.internal;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.resource.PathResourceResolver;
 
-import java.io.IOException;
 
 @Configuration
 class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/")
-                .resourceChain(true)
-                .addResolver(new PathResourceResolver() {
-                    @Override
-                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
-                        Resource resource = location.createRelative(resourcePath);
-                        if (resource.exists() && resource.isReadable()) {
-                            return resource;
-                        }
-
-                        if (!resourcePath.startsWith("assets/")
-                                && !resourcePath.startsWith("static/")) {
-                            return new ClassPathResource("/static/index.html");
-                        }
-
-                        return null;
-                    }
-                });
+        registry.addResourceHandler("/app/gen-me/public/**")
+                .addResourceLocations("classpath:/static/public/");
     }
 }
